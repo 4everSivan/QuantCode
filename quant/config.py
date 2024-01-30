41963ffb976872b044936ed716d3cd21043ca6c8
sivan <barry.shen@postgresdata.com>
2024-01-30 13:13:10 +0800

    update config and provide check method

--- quant/config.py
+++ quant/config.py
@@ -1,7 +1,9 @@
 import sys
 import yaml
 
-from typing import Dict
+from typing import Dict, Any, Optional
+from .tools import Tools
+
 
 class Config:
 
@@ -30,3 +32,41 @@ class Config:
                 file=sys.stderr
             )
             sys.exit(1)
+
+    def initialize(self) -> None:
+        if not self.config.get('general'):
+            print(
+                "Config error: General section is missing",
+                flush=True,
+                file=sys.stderr
+            )
+            sys.exit(1)
+
+        if not self.config['general'].get('target'):
+            print(
+                "Config error: general section params target is missing",
+                flush=True,
+                file=sys.stderr
+            )
+            sys.exit(1)
+
+        check_res = Tools.check_path(self.config['general']['target'], tp="dir")
+        if not check_res.get('state'):
+            print(
+                check_res.get('err_msg'),
+                flush=True,
+                file=sys.stderr
+            )
+            sys.exit(1)
+
+    def get(self, key: str, default: Optional[Any] = None) -> Any:
+        return self.config.get(key, default)
+
+    def keys(self) -> Any:
+        return self.config.keys()
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.config
+
+    def __getitem__(self, key: str) -> Any:
+        return self.config[key]
