710cb77e1f3e7f635fc11eee2a91799f34d25ee1
4everSivan <961014738@qq.com>
2024-01-30 14:12:11 +0800

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # quant_code
-Statistical code and graphical display
+Statistical code and graphical display.
+
+## Environment
+* Python 3.7+
+* Linux 7+ / MacOS 10.15+
--- quant/quant.py
+++ quant/quant.py
@@ -1,11 +1,44 @@
 import argparse
+import multiprocessing
+import os
 import sys
 
 from . import BANNER
+from .config import Config
+
+
+def worker(
+        p_id: int,
+        config: Config,
+        m_event: multiprocessing.Event,
+        m_queue: multiprocessing.JoinableQueue
+) -> None:
+    pass
+
+
+def run(config: Config) -> None:
+    # get target dir
+    all_dir = os.listdir(config['general'].get('target'))
+
+    for item in all_dir[:]:
+        if '.' in item:
+            all_dir.remove(item)
+
+    processes = []
+    event = multiprocessing.Event()
+    join_queue = multiprocessing.JoinableQueue()
+
+    for i in range(len(all_dir)):
+        processes.append(
+            multiprocessing.Process(
+                target=worker,
+                args=(i, config, event, join_queue)
+            )
+        )
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="QuantCode: statistics code number and graphical display. ")
+    parser = argparse.ArgumentParser(description="QuantCode: statistics code and graphical display. ")
     parser.add_argument(
         "-c",
         "--config",
@@ -20,7 +53,7 @@ def main() -> None:
         help="QuantCode version"
     )
 
-    args =  parser.parse_args()
+    args = parser.parse_args()
 
     if args.version:
         print(BANNER, flush=True)
@@ -32,3 +65,7 @@ def main() -> None:
         parser.parse_args(['-h'])
         sys.exit(0)
 
+    config = Config(config_path)
+    config.initialize()
+
+    run(config)
